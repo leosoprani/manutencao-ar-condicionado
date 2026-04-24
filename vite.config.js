c6f2c141ff846709e52555fb44f69da36150f924
leosoprani <leosoprani@msn.com>
2026-04-24 13:30:43 -0300

    Configura rota base do Vite para o GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/manutencao-ar-condicionado/',
-})
+  base: '/manutencao-ar-condicionado/'
+});
